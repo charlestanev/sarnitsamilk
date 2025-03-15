a2e432895862486c27adb0ae73418212df6596ad
ELMIR CHARLIE TANEV <51314768+charlestanev@users.noreply.github.com>
2025-03-15 18:29:33 +0200

    add: create AddProductPage component with initial state setup

---
+++ src/app/admin/add.tsx
@@ -0,0 +1,15 @@
+"use client";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+export default function AddProductPAge() {
+    const router = useRouter();
+
+    const [formData, setFormData] = useState({
+        name: "",
+        description: "",
+        price: "",
+        imageUrl: "",
+        categoryId: "",
+    });
+}
